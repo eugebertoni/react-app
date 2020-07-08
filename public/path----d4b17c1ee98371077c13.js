5129948ffeac5c0d246b7dcbee11ca3e0269dd9d
Eugenia Bertoni <eugenia@takefortytwo.com>
2020-07-08 16:14:06 +0200

    gatsby build

---
+++ public/path----d4b17c1ee98371077c13.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{107:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Design+Code 3"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----d4b17c1ee98371077c13.js.map
